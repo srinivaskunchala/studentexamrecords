eb2161e7f236f122a17f16d3034c896441ba4335
Kunchala <srinivas.kunchala@capgemini.com>
2020-08-11 21:42:39 -0400

    firstcommit1

--- src/main/java/com/example/demo/StudentExamRecordsApplication.java
+++ src/main/java/com/example/demo/StudentExamRecordsApplication.java
@@ -1,12 +1,13 @@
 package com.example.demo;
 
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+//import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+//import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
-@EnableEurekaClient
-@EnableDiscoveryClient
+//@EnableEurekaClient
+//@EnableDiscoveryClient
 @SpringBootApplication
 public class StudentExamRecordsApplication {
 
